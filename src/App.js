701e23b83a03a2a4f69b124a27df939fb6c55449
sowmyamummadi7411 <sowmyamummadi7411@gmail.com>
2023-05-31 14:59:50 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line import/extensions
+import MusicPlayList from './components/MusicPlayList'
+
+import './App.css'
+
+const App = () => <MusicPlayList />
+
+export default App
